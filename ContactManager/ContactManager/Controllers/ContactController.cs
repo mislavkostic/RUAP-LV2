066ce84432a102d103ae617b3d2bac017eedeb68
mislavkostic <mkostic@etfos.hr>
2021-11-15 16:02:56 +0100

    Task 2 - Creating the Contact Manager API Controllers

---
+++ ContactManager/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        // GET: Contact
+        public string[] Get()
+        {
+            return new string[]
+            {
+        "Hello",
+        "World"
+            };
+        }
+    }
+}
